75e3355e5fb3b25192d4b4aca3b10e0f1c71aad9
张利伟 <leewayzhang@outlook.com>
2017-12-07 20:57:17 +0800

    add project filter

--- src/app/utils/filter.pipe.ts
+++ src/app/utils/filter.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class FilterPipe implements PipeTransform {
 
   transform(items: any[], ...args: { field: string, value: string, negate: boolean }[]): any[] {
-    if (!items && items.length === 0) return [];
+    if (!items || items.length === 0) return [];
     for (let arg of args) {
       items = items.filter(item => arg.negate ? item[arg.field] !== arg.value : item[arg.field] === arg.value);
     }
